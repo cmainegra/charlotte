3ef0b5869a7499c3ec02fc71540b10d02b71a443
Charlie <cmainegra@protonmail.com>
2018-10-20 12:49:55 -0400

    trying again

---
+++ src/components/ScrapeNY/ScrapeNY.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ScrapeNY.css";
+
+const ScrapeNY = props => (
+    <div className="card">
+    <div className="container">
+        
+    </div>
+    </div>
+)
