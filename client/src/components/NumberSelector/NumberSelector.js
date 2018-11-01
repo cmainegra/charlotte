051edbfa21b06a342161d7c3de4b3f9deeef5452
Charlie <cmainegra@protonmail.com>
2018-11-01 17:31:47 -0400

    this isnt working

--- client/src/components/NumberSelector/NumberSelector.js
+++ client/src/components/NumberSelector/NumberSelector.js
@@ -4,43 +4,39 @@ import API from "../../utils/API";
 // import uuid from 'uuid';
 
 class NumberSelector extends Component {
-   
-    // state = {
-    //     numbers: [
-    //         { id: uuid(), num1: num1 },
-    //         { id: uuid(), num2: num2 },
-    //         { id: uuid(), num3: num3 },
-    //         { id: uuid(), num4: num4 },
-    //         { id: uuid(), num5: num5 },
-    //         { id: uuid(), num6: num6 }
-    //     ]
-    // }
-    
-    // randomize = event => {
-    //     event.preventDefault();
-    //     if(randomNum.length < 6) {
-    //         let randomNum = [];
-    //         numbers.push(randomNum) = Math.floor(Math.random() * 75)
-    //         this.randomize();
-    //     }else {
-    //         setState(numbers.push(randomNum))
-    //     }
+        
+    // constructor() {
+    //     super();
+    //     this.state = { numbers: [1,2,3,4,5,6] };
     // }
 
-    componentWillReceiveProps = event => {
-        this.setState({ num1: event.target.value.num1,  });
-    }
-    handleSubmit = event => {
-        event.preventDefault();
-        API.handleSubmit(event.target.num1.value, event.target.num2.value, event.target.num3.value, event.target.num4.value, event.target.num5.value, event.target.num6.value)
-        .then( response => {
-            console.log(response.data)
-        })
-        .catch( error => {
-            console.log(error);
-        });
-        console.log("submitted");
-    }
+
+    // state = {
+    //         numbers: [1,2,3,4,5,6]
+    //     }
+        
+        // randomize = event => {
+        //     event.preventDefault();
+        //     if(randomNum.length < 6) {
+        //         randomNum = Math.floor(Math.random() * Math.floor(75));
+        //         this.setState = numbers.push(randomNum);
+        //         this.randomize();
+        //     } else {
+        //         this.setState(this.state.numbers)
+        //     }
+        // }
+        handleSubmit = event => {
+            event.preventDefault();
+            API.handleSubmit(event.target.numbers)
+            .then( response => {
+                console.log(response.data)
+            })
+            .catch( error => {
+                console.log(error);
+            });
+            console.log("submitted");
+       }
+    
 
     render() {
         return(
@@ -53,11 +49,11 @@ class NumberSelector extends Component {
                 <input name="num5" value="num5" id="num" maxlength="2" min="1" max="75" type="number"></input>
                 <input name="num6" value="num6" id="num" maxlength="2" min="1" max="25" type="number"></input>
             </form>
-            <button>Randomize</button> 
+            <button onClick={this.randomize}>Randomize</button> 
             <button onClick={this.handleSubmit}>Submit</button>
            </div>
         );
-    }
-}
+        }
 
+    }
 export default NumberSelector;
